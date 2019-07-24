21b54a644f7e65aa891fdef63cce85d417e32fe0
mudasiryounas <mudasir@sarp.io>
2019-07-24 22:35:35 +0300

    .

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from models import User
 
 app = Flask(__name__)
 
-app.config.from_pyfile('config.cfg', silent=False)
+app.config.from_pyfile('config.cfg')
 
 db = SQLAlchemy()
 db.init_app(app)
 
 
-@app.route('/')
+@app.route('/test')
 def test():
-    return 'Hello World!'
+    return 'Hello World! I am from docker!'
+
 
 @app.route('/db_test')
-def test():
+def test_db():
+    db.create_all()
+    db.session.commit()
 
-    return 'Hello World!'
+    u = User(name='Mudasir', surname='Younas')
+    db.session.add(u)
+    db.session.commit()
+
+    user = User.query.first()
+    return "User '{} {}' is from database".format(user.name, user.surname)
