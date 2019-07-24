c151c49a8813b930d7e177dc8a0d75b3f3fdc232
mudasiryounas <mudasir@sarp.io>
2019-07-24 22:42:39 +0300

    .

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-from models import User
-
 app = Flask(__name__)
 
 app.config.from_pyfile('config.cfg')
@@ -11,19 +9,30 @@ db = SQLAlchemy()
 db.init_app(app)
 
 
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String())
+    surname = db.Column(db.String())
+
+
 @app.route('/test')
 def test():
     return 'Hello World! I am from docker!'
 
 
-@app.route('/db_test')
+@app.route('/test_db')
 def test_db():
     db.create_all()
     db.session.commit()
-
-    u = User(name='Mudasir', surname='Younas')
-    db.session.add(u)
-    db.session.commit()
-
+    user = User.query.first()
+    if not user:
+        u = User(name='Mudasir', surname='Younas')
+        db.session.add(u)
+        db.session.commit()
     user = User.query.first()
     return "User '{} {}' is from database".format(user.name, user.surname)
+
+
+if __name__ == '__main__':
+    app.run()
--- models.py
+++
@@ -1,7 +0,0 @@
-
-from app import db
-
-class User(db.Model):
-    __tablename__ = 'users'
-    name = db.Column(db.String())
-    surname = db.Column(db.String())
